573450c2a1efb2fac6bf6fed2f4bcdcd03b38cdf
Keagan Herman <Hermanleah75@gmail.com>
2022-11-23 22:59:31 +0200

    Update

--- app/src/main/java/com/example/wilfashionpage/activities/LoginActivity.java
+++ app/src/main/java/com/example/wilfashionpage/activities/LoginActivity.java
@@ -56,15 +56,12 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         auth.signInWithEmailAndPassword(userEmail,userPassword)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        }else{
-                            Toast.makeText(LoginActivity.this, "Error: "+task.getException(), Toast.LENGTH_SHORT).show();
-                        }
+                .addOnCompleteListener(LoginActivity.this, task -> {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Error: "+task.getException(), Toast.LENGTH_SHORT).show();
                     }
                 });
 
--- app/src/main/java/com/example/wilfashionpage/activities/PaymentActivity.java
+++ app/src/main/java/com/example/wilfashionpage/activities/PaymentActivity.java
@@ -1,17 +1,45 @@
 package com.example.wilfashionpage.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.interfaces.HttpResponseCallback;
+import com.braintreepayments.api.internal.HttpClient;
+import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.example.wilfashionpage.R;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
 
 public class PaymentActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1234;
+    final String API_GET_TOKEN ="https://localhost/braintree/main.php";
+    final String API_CHECK_OUT ="https://localhost/braintree/checkout.php";
+
+    String token;
+    HashMap<String, String> paramsHash;
     Toolbar toolbar;
     TextView subTotal, discount, shipping, total;
     Button paymentBtn;
@@ -43,17 +71,131 @@ public class PaymentActivity extends AppCompatActivity {
 
         subTotal.setText("R" + amount);
 
+        new getToken().execute();
+        
         paymentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                paymentMethod();
+                submitPayment();
             }
         });
 
-
-
     }
 
-    private void paymentMethod() {
+    private void submitPayment() {
+        DropInRequest dropInRequest = new DropInRequest().clientToken(token);
+        startActivityForResult(dropInRequest.getIntent(this), REQUEST_CODE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                PaymentMethodNonce nonce = result.getPaymentMethodNonce();
+                String strNonce = nonce.getNonce();
+
+                if (!subTotal.getText().toString().isEmpty())
+                {
+                    sendPayments();
+                }
+                else
+                {
+                    Toast.makeText(this, "Please select item", Toast.LENGTH_SHORT).show();
+                }
+
+
+            } else if (resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "User cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void sendPayments() {
+        RequestQueue queue = Volley.newRequestQueue(PaymentActivity.this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, API_CHECK_OUT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.toString().contains("Successful"))
+                            Toast.makeText(PaymentActivity.this, "Transaction successful", Toast.LENGTH_SHORT).show();
+                        else {
+                            Toast.makeText(PaymentActivity.this, "Transaction failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", error.toString());
+
+            }
+        })
+
+        {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if (paramsHash == null)
+                return null;
+                Map<String, String> params = new HashMap<>();
+                for (String key:params.keySet())
+                {
+                    params.put(key, paramsHash.get(key));
+                }
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        queue.add(stringRequest);
+    }
+
+    private class getToken extends AsyncTask {
+
+
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            HttpClient client = new HttpClient();
+            client.get(API_GET_TOKEN, new HttpResponseCallback() {
+                @Override
+                public void success(String responseBody) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            token = responseBody;
+                        }
+                    });
+                }
+
+                @Override
+                public void failure(Exception exception) {
+                    Log.d("ERROR", exception.toString());
+
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+
+        }
+    }
+
+
 }
